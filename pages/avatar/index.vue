60157e141e52de51a97267fabc4f304dd0423220
hacker233 <lhqhacker@163.com>
2022-01-14 15:05:57 +0800

    头像

---
+++ pages/avatar/index.vue
@@ -0,0 +1,73 @@
+<template>
+	<view class="">
+		<okingtz-cropper @uploadSuccess="uploadSuccess" saveButtonText="确认修改"></okingtz-cropper>
+	</view>
+</template>
+
+<script>
+	import {
+		updateAvatarAsync
+	} from "@/http/api/user.js"
+	import CONFIG from "@/config/index.js"
+	export default {
+		data() {
+			return {
+				avatar: ''
+			}
+		},
+		methods: {
+			// 上传头像
+			async uploadSuccess(item) {
+				console.log("up",CONFIG,updateAvatarAsync)
+				uni.showLoading({
+					title: '修改中'
+				});
+				// 上传文件
+				uni.uploadFile({
+					url: CONFIG.serverAddress + '/api/upload', // api地址
+					filePath: item, // 本地上传完成后的路径
+					name: 'file', // 默认
+					header: {
+						"Authorization": this.$store.state.token // token验证
+					},
+					formData: { // 其他的formdata参数
+					},
+					success: (res) => {
+						this.avatar = JSON.parse(res.data).data.fileUrl;
+						this.update();
+					},
+					fail: (res) => {
+						uni.showToast({
+							icon: "error",
+							title: "上传失败"
+						})
+					}
+				});
+			},
+			// 更新用户头像
+			async update() {
+				let params = {
+					avatar: this.avatar
+				}
+				console.log(this.avatar)
+				const data = await updateAvatarAsync(params);
+				if (data.code === "00000") {
+					uni.showToast({
+						icon: "success",
+						title: "修改成功"
+					})
+					uni.navigateBack({});
+					this.$store.dispatch("getUserInfo"); // 获取用户信息
+				} else {
+					uni.showToast({
+						icon: "error",
+						title: data.message
+					})
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/signature/index.vue
@@ -0,0 +1,81 @@
+<template>
+	<view class="signature-box">
+		<view class="input-box">
+			<u--input v-model="signature" maxlength="30" placeholder="有个性就有签名" border="bottom" clearable
+				:showWordLimit="true"></u--input>
+		</view>
+		<view class="save-box">
+			<view class="btn">
+				<u-button text="保存" size="mini" color="linear-gradient(to right, rgb(170, 170, 127), rgb(213, 51, 186))"
+					@click="saveSignature"></u-button>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import {
+		updateSignature
+	} from "@/http/api/user.js";
+	export default {
+		data() {
+			return {
+				signature: ''
+			}
+		},
+		onLoad(option) {
+			this.signature = option.oldSignature
+			console.log(option)
+		},
+		methods: {
+			// 保存签名
+			saveSignature() {
+				if (!this.signature) {
+					uni.showToast({
+						icon: "error",
+						title: "签名不能为空"
+					})
+				} else {
+					this.updateSignatureAsync();
+				}
+			},
+			async updateSignatureAsync() {
+				let params = {
+					signature: this.signature,
+				}
+				const data = await updateSignature(params);
+				if (data.code === "00000") {
+					uni.showToast({
+						icon: "success",
+						title: "修改成功"
+					})
+					uni.navigateBack({});
+					this.$store.dispatch("getUserInfo"); // 获取用户信息
+				} else {
+					uni.showToast({
+						icon: "error",
+						title: data.message
+					})
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.signature-box {
+		.input-box {
+			margin-bottom: 30rpx;
+		}
+
+		.save-box {
+			width: 100%;
+			display: flex;
+			justify-content: flex-end;
+
+			.btn {
+				width: 100rpx;
+			}
+		}
+	}
+</style>
