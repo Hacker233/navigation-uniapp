cd4bfaaecce31f2ba3fb58bf53996eac9b909b89
hacker233 <lhqhacker@163.com>
2022-02-14 19:33:35 +0800

    修复bug

--- utils/index.js
+++ utils/index.js
@@ -1,44 +1,66 @@
-export const showtime = (time) => {
-	let date =
-		typeof time === "number" ?
-		new Date(time) :
-		new Date((time || "").replace(/-/g, "/"));
-	let diff = (new Date().getTime() - date.getTime()) / 1000;
-	let dayDiff = Math.floor(diff / 86400);
+function getTs(time) {
+	var arr = time.split(/[- :]/),
+		_date = new Date(arr[0], arr[1] - 1, arr[2], arr[3], arr[4], arr[5]),
+		timeStr = Date.parse(_date)
+	return timeStr
+}
+export const showtime = (post_modified) => {
+	// 拿到当前时间戳和发布时的时间戳，然后得出时间戳差
+	var curTime = new Date();
+	var postTime = new Date(post_modified);
 
-	let isValidDate =
-		Object.prototype.toString.call(date) === "[object Date]" &&
-		!isNaN(date.getTime());
+	//部分浏览器不兼容此转换建议所以对此进行补充（指定调用自己定义的函数进行生成发布时间的时间戳）
 
-	if (!isValidDate) {
-		window.console.error("不是有效日期格式");
+	//var timeDiff = curTime.getTime() - postTime.getTime();
+	//上面一行代码可以换成以下（兼容性的解决）
+	var timeDiff = curTime.getTime() - getTs(post_modified);
+
+	// 单位换算
+	var min = 60 * 1000;
+	var hour = min * 60;
+	var day = hour * 24;
+	var week = day * 7;
+	var month = week * 4;
+	var year = month * 12;
+
+	// 计算发布时间距离当前时间的周、天、时、分
+	var exceedyear = Math.floor(timeDiff / year);
+	var exceedmonth = Math.floor(timeDiff / month);
+	var exceedWeek = Math.floor(timeDiff / week);
+	var exceedDay = Math.floor(timeDiff / day);
+	var exceedHour = Math.floor(timeDiff / hour);
+	var exceedMin = Math.floor(timeDiff / min);
+
+
+	// 最后判断时间差到底是属于哪个区间，然后return
+
+	if (exceedyear < 100 && exceedyear > 0) {
+		return '发表于' + exceedyear + '年前';
+	} else {
+		if (exceedmonth < 12 && exceedmonth > 0) {
+			return exceedmonth + '月前';
+		} else {
+			if (exceedWeek < 4 && exceedWeek > 0) {
+				return exceedWeek + '周前';
+			} else {
+				if (exceedDay < 7 && exceedDay > 0) {
+					return exceedDay + '天前';
+				} else {
+					if (exceedHour < 24 && exceedHour > 0) {
+						return exceedHour + '小时前';
+					} else {
+						if (exceedMin == 0) {
+							return '刚刚发表';
+						} else {
+							return exceedMin + '分钟前';
+						}
+
+					}
+				}
+			}
+		}
 	}
-	const formatDate = function(date) {
-		let today = new Date(date);
-		let year = today.getFullYear();
-		let month = ("0" + (today.getMonth() + 1)).slice(-2);
-		let day = ("0" + today.getDate()).slice(-2);
-		let hour = today.getHours();
-		let minute = today.getMinutes();
-		let second = today.getSeconds();
-		// return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
-		return `${year}-${month}-${day}`;
-	};
-	//小于0或者大于等于31显示原时间
-	if (isNaN(dayDiff) || dayDiff < 0 || dayDiff >= 31) {
-		return formatDate(date);
-	}
-	return (
-		(dayDiff === 0 &&
-			((diff < 60 && "刚刚") ||
-				(diff < 120 && "1分钟前") ||
-				(diff < 3600 && Math.floor(diff / 60) + "分钟前") ||
-				(diff < 7200 && "1小时前") ||
-				(diff < 86400 && Math.floor(diff / 3600) + "小时前"))) ||
-		(dayDiff === 1 && "昨天") ||
-		(dayDiff < 7 && dayDiff + "天前") ||
-		(dayDiff < 31 && Math.ceil(dayDiff / 7) + "周前")
-	);
+
 }
 
 // 将base64转换为文件
@@ -55,3 +77,36 @@ export const dataURLtoFile = (dataurl, filename) => {
 		type: mime
 	});
 }
+
+
+// 函数节流
+/**
+ * 节流
+ * @param {Function} func 要执行的回调函数 
+ * @param {Number} wait 延时的时间
+ * @param {Boolean} immediate 是否立即执行
+ * @return null
+ */
+let timer, flag;
+export function throttle(func, wait = 300, immediate = true) {
+	if (immediate) {
+		if (!flag) {
+			flag = true;
+			// 如果是立即执行，则在wait毫秒内开始时执行
+			typeof func === 'function' && func();
+			timer = setTimeout(() => {
+				flag = false;
+			}, wait);
+		}
+	} else {
+		if (!flag) {
+			flag = true
+			// 如果是非立即执行，则在wait毫秒内的结束处执行
+			timer = setTimeout(() => {
+				flag = false
+				typeof func === 'function' && func();
+			}, wait);
+		}
+
+	}
+};
